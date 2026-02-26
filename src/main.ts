d9ab29732521035b6a88f490c7c1e00998da85be
xorcuit <xorcuit@gmail.com>
2026-02-26 07:22:34 +0700

    fix: remove user info usage & upgrade sdk

--- src/main.ts
+++ src/main.ts
@@ -31,9 +31,7 @@ if (!input.profiles?.length) {
 
 const { actorId, actorRunId, actorBuildId, userId, actorMaxPaidDatasetItems, memoryMbytes } =
   Actor.getEnv();
-
-const client = Actor.newClient();
-const user = userId ? await client.user(userId).get() : null;
+const isPaying = !!process.env.APIFY_USER_IS_PAYING;
 
 const scraper = createLinkedinScraper({
   apiKey: process.env.HARVESTAPI_TOKEN!,
@@ -45,8 +43,8 @@ const scraper = createLinkedinScraper({
     'x-apify-actor-build-id': actorBuildId!,
     'x-apify-memory-mbytes': String(memoryMbytes),
     'x-apify-actor-max-paid-dataset-items': String(actorMaxPaidDatasetItems) || '0',
-    'x-apify-username': user?.username || '',
-    'x-apify-user-is-paying': (user as Record<string, any> | null)?.isPaying,
+    'x-apify-user-is-paying': String(isPaying),
+    'x-apify-user-is-paying-2': process.env.APIFY_USER_IS_PAYING || '',
   },
 });
 
